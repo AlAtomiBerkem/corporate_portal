08f64abf31e139908c48d85882fc2a01c85e0aaf
annapirz <fanworld991@gmail.com>
2025-04-23 19:32:15 +0300

    new server

--- server/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    JWT_SECRET: 'blog_secret_2024_!@#$%^&*()_min_32_chars', // В продакшене используйте process.env
-    ADMIN_INITIAL_PASSWORD: 'admin123' // Пароль для первого входа
-};
---
+++ server/controllers/authController.js
@@ -0,0 +1,25 @@
+class AuthController {
+
+    async register (req, res) {
+        try {
+
+        } catch (e) {
+        }
+    }
+
+    async news (req, res) {
+        try {
+            res.json('все работает')
+        } catch (e) {
+        }
+    }
+
+    async login (req, res) {
+        try {
+
+        } catch (e) {
+        }
+    }
+}
+
+module.exports = new AuthController;
