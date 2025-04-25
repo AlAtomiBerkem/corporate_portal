bddd4d86d908addd46370478e00ecbcdb0a0b63c
annapirz <fanworld991@gmail.com>
2025-04-25 15:19:02 +0300

    add publick routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,4 +7,7 @@ router.post('/login', authController.login);
 // POST /auth/refresh → вызывает authController.refresh
 router.post('/refresh', authController.refresh);
 
+//POST запрос на выход администратора из системы
+// router.post( '/logout', authController.logout)
+
 module.exports = router;
