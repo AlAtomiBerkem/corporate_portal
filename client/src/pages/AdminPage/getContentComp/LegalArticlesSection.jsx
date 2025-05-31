eabfeb536b7f19815169dbd97bf5b2d60c8e3e6c
annapirz <fanworld991@gmail.com>
2025-05-31 17:23:13 +0300

    feat: content editor add

--- client/src/pages/AdminPage/getContentComp/LegalArticlesSection.jsx
+++ client/src/pages/AdminPage/getContentComp/LegalArticlesSection.jsx
@@ -1,4 +1,4 @@
-import ContentSection from '../../../hooks/useContentSection.jsx';
+import UseContentSection from '../../../hooks/useContentSection.jsx';
 import ContentEditor from '../createContentComp/ContentEditor.jsx';
 import { publicApi } from "../../../api/publicApi.js";
 import { adminApi } from "../../../api/adminApi.js";
@@ -16,7 +16,7 @@ const LegalArticlesSection = () => {
     )
 
     return (
-        <ContentSection
+        <UseContentSection
             apiMethods={{
                 get: publicApi.getLegalArticle,
                 create: adminApi.createLegal,
