258d3a144a1c34881d73d3427684b005d2421f43
annapirz <fanworld991@gmail.com>
2025-06-22 23:07:17 +0300

    DOCKER

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Чтобы сервер слушал все интерфейсы
+    watch: {
+      usePolling: true,  // Нужно для работы в Docker
+    },
+  }
 })
