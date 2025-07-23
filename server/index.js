2e82505056900005e102d3f9f3945caffd3f8701
annapirz <fanworld991@gmail.com>
2025-07-23 18:46:38 +0300

    feat: add hot reload

--- server/index.js
+++ server/index.js
@@ -7,8 +7,10 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost', 'http://localhost:80'],
-    credentials: true
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(cookieParser());
