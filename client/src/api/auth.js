53330b70072fa71a4c3eb2982c474990e48f4695
annapirz <fanworld991@gmail.com>
2025-07-23 18:32:27 +0300

    feat: relize done version

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,6 @@
 import { refreshToken } from '../helpers/Auth.js'
 
-const baseUrl = 'http://localhost:5000';
+const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 export async function fetchAPI(endpoint, method = 'GET', body = null, needAuth = false, isFileDownload = false) {
     const headers = {};
