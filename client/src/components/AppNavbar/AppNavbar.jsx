04901920402b777c23ade8a7c84d3576cca52175
annapirz <fanworld991@gmail.com>
2025-08-19 16:11:28 +0300

    fix: ancor 3

--- client/src/components/AppNavbar/AppNavbar.jsx
+++ client/src/components/AppNavbar/AppNavbar.jsx
@@ -1,13 +1,37 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './AppNavbarStyle.css';
 import { HashLink } from 'react-router-hash-link';
 
 const AppNavbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const location = useLocation();
+    const navigate = useNavigate();
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
+    };
+
+    const handleContactsClick = (e) => {
+        e.preventDefault();
+        setIsMenuOpen(false);
+        if (location.pathname !== '/') {
+            navigate('/', { state: { scrollToBottom: true } });
+        } else {
+            window.scrollTo({ top: document.documentElement.scrollHeight, behavior: 'smooth' });
+        }
+    };
+
+    const handleMaintenanceClick = (e) => {
+        e.preventDefault();
+        setIsMenuOpen(false);
+        const anchorId = 'maintenanceAncor';
+        if (location.pathname !== '/') {
+            navigate('/', { state: { scrollToId: anchorId } });
+        } else {
+            const el = document.getElementById(anchorId);
+            if (el) el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
     };
 
     return (
@@ -30,7 +54,7 @@ const AppNavbar = () => {
                         <Link to="/PageContent" className="navigate__link">Подключение газа</Link>
                     </li>
                     <li className="navigate__item">
-                        <HashLink smooth to="/#maintenanceAncor" className="navigate__link">Техническое обслуживание</HashLink>
+                        <Link to="/" onClick={handleMaintenanceClick} className="navigate__link">Техническое обслуживание</Link>
                     </li>
                     <li className="navigate__item">
                         <Link to="/PageLegal" className="navigate__link">Юридическим лицам</Link>
@@ -39,7 +63,7 @@ const AppNavbar = () => {
                         <Link to="/documents" className="navigate__link">Прилагающиеся документы</Link>
                     </li>
                     <li className="navigate__item">
-                        <HashLink smooth to="/#contactAncor" className="navigate__link">Контакты</HashLink>
+                        <Link to="/" onClick={handleContactsClick} className="navigate__link">Контакты</Link>
                     </li>
                 </ul>
             </div>
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppHeader from "../components/AppHeader/AppHeader.jsx";
 import AppNavbar from "../components/AppNavbar/AppNavbar.jsx";
 import AppHeroBlock from "../components/AppHeroBlock/AppHeroBlock.jsx";
@@ -11,7 +11,23 @@ import FAQ from "../components/AppFAQ/AppFAQ.jsx";
 
 import NewsList from "../components/NewsList/NewsList.jsx";
 
-const MainPage = () => {
+const MainPage = ({}) => {
+    useEffect(() => {
+        const usr = window.history.state && window.history.state.usr;
+        if (usr?.scrollToBottom) {
+            setTimeout(() => {
+                window.scrollTo({ top: document.documentElement.scrollHeight, behavior: 'smooth' });
+            }, 0);
+            return;
+        }
+        if (usr?.scrollToId) {
+            setTimeout(() => {
+                const el = document.getElementById(usr.scrollToId);
+                if (el) el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            }, 0);
+        }
+    }, []);
+
     return (
         <div>
             <AppHeader />
